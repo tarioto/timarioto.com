c3992a6de7ede058faaa2a98ab2435767ced23d6
Tim Arioto <timarioto@gmail.com>
2020-09-18 13:42:20 +0200

    add github icon

--- src/app/modules/fontawesome-icons.module.ts
+++ src/app/modules/fontawesome-icons.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faArrowDown } from '@fortawesome/pro-duotone-svg-icons';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @NgModule({
   declarations: [],
@@ -13,7 +13,6 @@ import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
 export class FontawesomeIconsModule {
   constructor(library: FaIconLibrary) {
     // Add an icon to the library for convenient access in other components
-    library.addIcons(faArrowDown);
-    library.addIcons(faLinkedin);
+    library.addIcons(faArrowDown, faLinkedin, faGithub);
   }
 }
